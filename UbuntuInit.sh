24adfba7cba89603467ed575ecebeeb231cdae4a
Zephyrols <qiu_zeyuan@126.com>
2024-10-18 03:02:41 +0800

    Add .vimrc file with Vim configuration settings

--- UbuntuInit.sh
+++ UbuntuInit.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# 获取脚本自身所在的文件夹
+SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
+echo "脚本所在的文件夹是: $SCRIPT_DIR"
+
 bash ./ConfigSourelist.sh
 bash ./RemoveSnap.sh
 bash ./InstallAnaconda.sh
+
+cp $SCRIPT_DIR/.vimrc $HOME/.vimrc
