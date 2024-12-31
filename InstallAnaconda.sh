98ef36df8e44f69d0c2a2360f5f286f7aaebc791
zephyrols <qiu_zeyuan@126.com>
2025-01-01 00:18:23 +0800

    更新 Anaconda 安装脚本，改为下载 Miniconda 安装程序

--- InstallAnaconda.sh
+++ InstallAnaconda.sh
@@ -1,5 +1,5 @@
 # 下载 Anaconda 安装脚本
-wget https://repo.anaconda.com/archive/Anaconda3-2023.03-Linux-x86_64.sh -O anaconda.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O anaconda.sh
 
 # 运行安装脚本
 bash anaconda.sh -b -p $HOME/anaconda3
